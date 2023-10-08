44b50382c18e2f66ea6da0f7d650996e4e025ffc
fabricio <fabriciofarias9118@gmail.com>
2023-10-08 17:22:59 -0300

    Implemenatação Full Screen

--- app/src/main/java/com/example/game_2d/MainActivity.java
+++ app/src/main/java/com/example/game_2d/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +15,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        try {
+            getSupportActionBar().hide();
+        } catch (Exception e) {
+            Log.e("LogGame", "oonCreat: "+e.getMessage());
+        }
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
 
         constraintLayout = findViewById(R.id.container);
 
