b5b917dd86f2fdf08d77bc6c24cfb9a191bc1777
fabricio <fabriciofarias9118@gmail.com>
2023-10-08 17:06:49 -0300

    Criação da tela grafica com SurfaceView

---
+++ app/src/main/java/com/example/game_2d/GameEngine.java
@@ -0,0 +1,10 @@
+package com.example.game_2d;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameEngine extends SurfaceView {
+    public GameEngine(Context context) {
+        super(context);
+    }
+}
