8bb07ce896dd7772ec67726f341db2d4e449ade4
fabricio <fabriciofarias9118@gmail.com>
2023-10-08 23:47:06 -0300

    IMplementação da classe Cores

---
+++ app/src/main/java/com/example/game_2d/Cores.java
@@ -0,0 +1,20 @@
+package com.example.game_2d;
+
+import android.graphics.Paint;
+
+public class Cores {
+
+    public static Paint corTextoPontos(){
+        Paint corTextoPontos = new Paint();
+        corTextoPontos.setColor(0xFFFF0000);
+
+        return corTextoPontos;
+    }
+
+    public static Paint corGameOver(){
+        Paint corGameOver = new Paint();
+        corGameOver.setColor(0x0000FFFF);
+
+        return corGameOver;
+    }
+}
--- app/src/main/java/com/example/game_2d/GameEngine.java
+++ app/src/main/java/com/example/game_2d/GameEngine.java
@@ -4,7 +4,14 @@ import android.content.Context;
 import android.view.SurfaceView;
 
 /*
-*  Game Engine
+ *  TODO: Clase Core
+ *  TODO: Clase Sons
+ *  TODO: Clase Obstaculo
+ *  TODO: Clase Obstaculos **
+ *  TODO: Clase Colisões
+ *  TODO: Clase Pontuação
+ *  TODO: Clase Game Over
+ *  TODO: Clase Cenario do game (Tela - UI/UX)
  */
 
 public class GameEngine extends SurfaceView {
