2d198fb981d5c985bcfd308cbdbfd5bc778b568c
fabricio <fabriciofarias9118@gmail.com>
2023-10-08 23:35:07 -0300

    Game Engine criação de estrutura da classe

--- app/src/main/java/com/example/game_2d/GameEngine.java
+++ app/src/main/java/com/example/game_2d/GameEngine.java
@@ -3,6 +3,10 @@ package com.example.game_2d;
 import android.content.Context;
 import android.view.SurfaceView;
 
+/*
+*  Game Engine
+ */
+
 public class GameEngine extends SurfaceView {
     public GameEngine(Context context) {
         super(context);
